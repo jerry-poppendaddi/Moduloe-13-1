d49b7bf3a8e72b74ed89a6f835d76f0dd9c02f2b
Ive Alex <ive.alex.89@gmail.com>
2024-03-27 20:42:49 -0400

    Add project files.

---
+++ Moduloe 13-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "C:/Users/iveal/Downloads/Text1.txt";
+
+        // Читаем весь текст
+        string[] words = File.ReadAllText(filePath).Split(new[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Создаем List<T> и LinkedList<T> для слов
+        List<string> list = new List<string>(words);
+        LinkedList<string> linkedList = new LinkedList<string>(words);
+
+        // Таймер для List<T>
+        Stopwatch listStopwatch = Stopwatch.StartNew();
+        ProcessWords(list);
+        listStopwatch.Stop();
+
+        // Таймер для LinkedList<T>
+        Stopwatch linkedListStopwatch = Stopwatch.StartNew();
+        ProcessWords(linkedList);
+        linkedListStopwatch.Stop();
+
+        // Результат
+        Console.WriteLine("List<T> processing time: " + listStopwatch.Elapsed);
+        Console.WriteLine("LinkedList<T> processing time: " + linkedListStopwatch.Elapsed);
+    }
+
+    static void ProcessWords(IEnumerable<string> words)
+    {
+        // Выполняем простую операцию для подсчета времени
+        foreach (var word in words)
+        {
+           int length = word.Length;
+        }
+    }
+}
